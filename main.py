96e87e2f93f844e4aa2920519486c0cb5440d095
Gabriel Braz <gabrielbraz@FRANQ.COM>
2025-08-12 15:13:14 -0300

    Primeiro commit da API FastAPI

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import os
+from dotenv import load_dotenv
+
+app = FastAPI()
+load_dotenv()
+
+def conectar_pg():
+    return psycopg2.connect(
+        host=os.getenv("PG_HOST"),
+        port=os.getenv("PG_PORT"),
+        database=os.getenv("PG_DB"),
+        user=os.getenv("PG_USER"),
+        password=os.getenv("PG_PASSWORD"),
+        cursor_factory=RealDictCursor
+    )
+
+@app.get("/dados")
+def obter_dados():
+    try:
+        conn = conectar_pg()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM tb_integrante limit 100")  # Edite aqui
+        dados = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return dados
+    except Exception as e:
+        return {"erro": str(e)}
+
